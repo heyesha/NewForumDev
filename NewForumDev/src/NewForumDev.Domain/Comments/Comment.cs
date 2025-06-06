61d66bf5462abe68adeef6352ba4016d273473d8
Rodion <rodion.kondrashin@bk.ru>
2025-06-06 19:58:55 +0700

    Done with main Domain models

---
+++ NewForumDev/src/NewForumDev.Domain/Comments/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace NewForumDev.Domain.Comments;
+
+public class Comment
+{
+    public Guid Id { get; set; }
+    
+    public required Guid UserId { get; set; }
+    
+    public Comment? Parent { get; set; }
+    
+    public required Guid EntityId { get; set; }
+    
+    public required string EntityType { get; set; }
+    
+    public List<Comment> Children { get; set; } = [];
+}
